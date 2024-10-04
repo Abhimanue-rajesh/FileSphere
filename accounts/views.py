a5dc439e5e755bf87e82ddccc53e17d627dafef7
abhimanue <abhimanuemvk@gmail.com>
2024-10-04 11:17:34 +0530

    forms and minor changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from .forms import CustomAuthenticationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "registration/login.html"
 
 
 class SignUpView(CreateView):
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic.base import TemplateView
 from django.views.static import serve
+from accounts.views import CustomLoginView
 
 urlpatterns = [
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
@@ -10,5 +11,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
